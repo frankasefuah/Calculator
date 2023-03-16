c3b7d046488cbdaa9050b816b719e406ffdd2776
Frank Asefuah <asefuahfrank@gmail.com>
2023-03-16 22:14:15 +0000

    Pseudocode for JS

---
+++ main.js
@@ -0,0 +1,9 @@
+// PseudoCode
+// accept user input
+// should accept decimals
+// store input from user
+// recognise input and perform calculations
+// result
+// C should clear the screen
+// store previous total as start of next operations
+// prevent invalid inputs (operators next to each other , two decimals next to each other)
