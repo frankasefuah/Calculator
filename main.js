a8fe62570c3c6e5356a6667d7c3fb7af12df63c6
Frank Asefuah <asefuahfrank@gmail.com>
2023-03-20 20:24:47 +0000

    Js for the calculator

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// PseudoCode
+//TODO PseudoCode:
 // accept user input
 // should accept decimals
 // store input from user
@@ -13,5 +13,33 @@ const keys = document.querySelector('.calculator-btns')
 keys.addEventListener('click', event => {
     const target = event.target
     const value = target.value
-    !target.matches('button') ? 0 : console.log(value)
+    !target.matches('button') ? 0 : calculator.parseInput(value)
 })
+
+const calculator = {
+    displayText: '0',
+    prevTotal: null,
+    parseInput(value) {
+        if (this.displayText === '0') {
+            this.displayText = ''
+        }
+        // have any of the special buttons been clicked
+        switch (value) {
+            case '=':
+                // calculate the answer
+                break
+            case 'C':
+                // clear screen and stored values
+                break
+            case '.':
+                if (this.displayText === 0) {
+                    // pass 0. into the add text method
+                } else {
+                    // add the decimal
+                }
+                break
+
+
+        }
+    }
+}
